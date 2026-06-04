30aaa31d80e3cda5a1546124139f44a1bd1db42b
doxcom <aldoperez1319@gmail.com>
2026-06-04 02:22:58 -0600

    practice and understanding embeddings semantic

---
+++ EmbeddingVector.py
@@ -0,0 +1,18 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+#frases
+
+frase1 = "El perro corre feliz por el parque"
+frase2 = "El canino disfruta corriendo en la plaza"
+frase3 = "Me gusta programar en python"
+
+#Generar embeddings(vectores numericos)
+
+emb1 = model.encode(frase1)
+emb2 = model.encode(frase2)
+emb3 = model.encode(frase3)
+
+print(f"Longitud del vector: {len(emb1)}")  #dimensions..
+print(f"Primeros 10 numeros del vector: {emb1[:10]}")  #0.23, -0.45
