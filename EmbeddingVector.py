792df1ceb489ea1b5ff6af0c7672c66aa7fd4546
doxcom <aldoperez1319@gmail.com>
2026-06-05 01:29:27 -0600

    mod embed

--- EmbeddingVector.py
+++ EmbeddingVector.py
@@ -15,4 +15,7 @@ emb2 = model.encode(frase2)
 emb3 = model.encode(frase3)
 
 print(f"Longitud del vector: {len(emb1)}")  #dimensions..
-print(f"Primeros 10 numeros del vector: {emb1[:10]}")  #0.23, -0.45
+print(f"Primeros 10 numeros del 1er vector: {emb1[:10]}")  #0.23, -0.45
+print(f"Primeros 10 numeros del 2do vector: {emb2[:10]}")  #0.23, -0.45
+print(f"Primeros 10 numeros del 3er vector: {emb3[:10]}")  #0.23, -0.45
+
